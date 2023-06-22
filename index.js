2ec1766ae007382ea51a97d72dc97f3faec50c61
Abel Bejiga <104611692+abel-bejiga@users.noreply.github.com>
2023-06-21 22:27:22 -0400

    minor bug fix

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-// scroll snap
-
-const scrollContainer = document.getElementById('mainCont');
-
-scrollContainer.addEventListener('scroll', handleScroll);
-
-function handleScroll() {
-  const scrollThreshold = 0.5;
-  const scrollPosition = scrollContainer.scrollTop;
-  const scrollThresholdPixels = scrollThreshold * scrollContainer.clientHeight;
-
-  scrollContainer.querySelectorAll('.scroll-item').forEach((item) => {
-    const itemOffsetTop = item.offsetTop;
-    const distance = Math.abs(itemOffsetTop - scrollPosition);
-
-    if (distance <= scrollThresholdPixels) {
-      scrollContainer.scrollTo({
-        top: itemOffsetTop,
-        behavior: 'smooth',
-      });
-    }
-  });
-}
-
 // modal menu 
 let menu = document.getElementById('menu')
 let close = document.getElementById('close-btn')
